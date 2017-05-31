f6caec4817344e1ae13a22a30fef0b4435b7ce0e
zyliu <i@zyliu.com>
2017-05-31 19:10:08 +0800

    Add example project

---
+++ Example/EnableViewDebuggingExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  EnableViewDebuggingExample
+//
+//  Created by 刘子洋 on 2017/5/31.
+//  Copyright © 2017年 刘子洋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Example/EnableViewDebuggingExample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  EnableViewDebuggingExample
+//
+//  Created by 刘子洋 on 2017/5/31.
+//  Copyright © 2017年 刘子洋. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
